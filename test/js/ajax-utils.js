6e84ff7dddb3fd0e626afe4a2638c0daba26e0d9
Sameer <sameer@mathad.com>
2017-11-15 14:37:03 +0530

    test

--- test/js/ajax-utils.js
+++ test/js/ajax-utils.js
@@ -23,11 +23,11 @@ function getRequestObject() {
 
 // Makes an Ajax GET request to 'requestUrl'
 ajaxUtils.sendGetRequest =
-  function(requestUrl, responseHandler) {
+  function(requestUrl, responseHandler, isJsonresponse) {
     var request = getRequestObject();
     request.onreadystatechange =
       function() {
-        handleResponse(request, responseHandler);
+        handleResponse(request, responseHandler,isJsonresponse);
       };
     request.open("GET", requestUrl, true);
     request.send(null); // for POST only
@@ -38,16 +38,27 @@ ajaxUtils.sendGetRequest =
 // function if response is ready
 // and not an error
 function handleResponse(request,
-                        responseHandler) {
+                        responseHandler,isJsonresponse) {
   if ((request.readyState == 4) &&
      (request.status == 200)) {
-    responseHandler(request);
+
+  if(isJsonresponse == undefined){
+    isJsonresponse = true;
+  }
+  if(isJsonresponse){
+    responseHandler(JSON.parse(request.responseText));
+  }
+  else {
+    responseHandler(request.responseText);
+    }
   }
 }
 
 
 // Expose utility to the global object
+//global.$ajaxUtils = ajaxUtils;
 global.$ajaxUtils = ajaxUtils;
+
 
 
 })(window);
--- test/js/script.js
+++ test/js/script.js
@@ -8,13 +8,26 @@ document.addEventListener("DOMContentLoaded",
 
         // Call server to get the name
         $ajaxUtils
-          .sendGetRequest("data/name.txt",
-            function (request) {
-              var name = request.responseText;
-              console.log(name);
+          .sendGetRequest("data/name.json",
+            function (res) {
+              console.log(res["no of cars"]);
+              var message = res.firstname + " " +res.lastname ;
+              if (res.likescurd){
+                message += " likes curd"
+              }
+              else {
+                message += "doesn't like curd"
+              }
+              message += " and has "
+              message += res.noofpens
+              message += " pens to write and has "
+              message += res["no of cars"]
+              message += ' car'
 
-              document.querySelector("content")
-                .innerHTML = "<h2>Hello " + name + "!</h2>";
+
+
+              document.querySelector("#content")
+                .innerHTML = "<h2>Hello "+ message +" !</h2>";
             });
 
 
